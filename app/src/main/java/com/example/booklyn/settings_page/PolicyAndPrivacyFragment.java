92ba4c942c027636ed41dc3e631e180c170e1e59
Ivan <defoxyweex@gmail.com>
2023-04-16 18:53:51 +0300

    Сделана страница настроек

---
+++ app/src/main/java/com/example/booklyn/settings_page/PolicyAndPrivacyFragment.java
@@ -0,0 +1,21 @@
+package com.example.booklyn.settings_page;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.booklyn.R;
+
+public class PolicyAndPrivacyFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_policy_and_privacy, container, false);
+    }
+}
--- app/src/main/java/com/example/booklyn/settings_page/SettingsPageFragment.java
+++ app/src/main/java/com/example/booklyn/settings_page/SettingsPageFragment.java
@@ -2,65 +2,61 @@ package com.example.booklyn.settings_page;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.booklyn.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link SettingsPageFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class SettingsPageFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public SettingsPageFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SettingsPageFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static SettingsPageFragment newInstance(String param1, String param2) {
-        SettingsPageFragment fragment = new SettingsPageFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_settings_page, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        Button button = view.findViewById(R.id.privacy_and_policy_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Navigation.findNavController(button).navigate(R.id.action_settingsPageFragment_to_policyAndPrivacyFragment);
+            }
+        });
+
+        Button button1 = view.findViewById(R.id.terms_and_conditions_button);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Navigation.findNavController(button1).navigate(R.id.action_settingsPageFragment_to_termsAndConditionsFragment);
+            }
+        });
+
+        Button button2 = view.findViewById(R.id.about_the_app_button);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Navigation.findNavController(button2).navigate(R.id.action_settingsPageFragment_to_aboutAppFragment);
+            }
+        });
+
+        Button button3 = view.findViewById(R.id.support_button);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Navigation.findNavController(button3).navigate(R.id.action_settingsPageFragment_to_supportFragment);
+            }
+        });
     }
 }
